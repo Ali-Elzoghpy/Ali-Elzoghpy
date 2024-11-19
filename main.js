f6f099f6edf7592d627031e9925c9dc60e51c718
Ali-Elzoghpy <168960345+Ali-Elzoghpy@users.noreply.github.com>
2024-11-20 00:41:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+
+
+
+function displayQut() {
+
+    var randomNumber = Math.floor(Math.random() * 11)
+    if (randomNumber == 0) {
+        document.getElementById("demo").innerHTML = `“Be yourself; everyone else is already taken.”
+― Oscar Wilde`
+
+
+    }
+    else if (randomNumber == 1) {
+        document.getElementById("demo").innerHTML = `“So many books, so little time.”
+― Frank Zappa`
+
+    }
+    else if (randomNumber == 2) {
+        document.getElementById("demo").innerHTML = `“Be You And Dont Be a Copy Of others ”`
+
+    }
+    else if (randomNumber == 3) {
+        document.getElementById("demo").innerHTML = `"The best revenge is massive success."
+
+--Frank Sinatra`
+
+    }
+    else if (randomNumber == 4) {
+        document.getElementById("demo").innerHTML = ` “You Must Know That There Is No finish Line in Programming ”`
+
+    }
+    else if (randomNumber == 5) {
+        document.getElementById("demo").innerHTML = ` “You Must Know That There Is No finish Line in Programming ”`
+
+    }
+    else if (randomNumber == 6) {
+        document.getElementById("demo").innerHTML = ` “ Manage your time Becuse its your Real Enemy If you did Not Manage It ”`
+
+    }
+    else if (randomNumber == 7) {
+        document.getElementById("demo").innerHTML = ` “ The Real Frind is That The One Who Push You Forwared not Pull You Down  ”`
+
+    }
+    else if (randomNumber == 8) {
+        document.getElementById("demo").innerHTML = ` “  There IS No Words To Say  ”`
+
+    }
+
+
+}
+
